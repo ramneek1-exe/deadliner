e40d9db14504ee26a2c1d96878bb50fcd88668ab
ramneek1-exe <ramneek88888@gmail.com>
2026-02-18 16:18:57 -0700

    mobile icons

---
+++ app/manifest.ts
@@ -0,0 +1,26 @@
+
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: "Deadliner",
+        short_name: "Deadliner",
+        description: "Syllabus to calendar in seconds.",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#000000",
+        theme_color: "#000000",
+        icons: [
+            {
+                src: "/favicon-dark.svg",
+                sizes: "any",
+                type: "image/svg+xml",
+            },
+            {
+                src: "/apple-icon",
+                sizes: "180x180",
+                type: "image/png",
+            },
+        ],
+    };
+}
